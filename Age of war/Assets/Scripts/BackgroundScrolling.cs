645976470884a75eec3646825ac92aa88a79e109
myannisbg <503p2.yannis.mercier@gmail.com>
2024-01-25 09:49:11 +0100

    background GD

--- Age of war/Assets/Scripts/BackgroundScrolling.cs
+++ Age of war/Assets/Scripts/BackgroundScrolling.cs
@@ -6,6 +6,7 @@ public class BackgroundScrolling : MonoBehaviour
     public float scrollSpeed = 0.5f;
     private float offset;
     private Material mat;
+    private bool direction = false;
 
     void Start()
     {
@@ -13,8 +14,20 @@ public class BackgroundScrolling : MonoBehaviour
     }
 
     void Update()
-    {
-        offset += (Time.deltaTime * scrollSpeed) / 10f;
+    {   
+        Debug.Log(offset);
+        if (offset > 0.5f ){
+            direction = true;
+        }
+        else if (offset < 0f ){
+            direction = false;
+        }
+        if (direction == false){
+            offset += (Time.deltaTime * scrollSpeed) / 10f;
+        }
+        else{
+            offset -= (Time.deltaTime * scrollSpeed) / 10f;
+        }
         mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
     }
 }

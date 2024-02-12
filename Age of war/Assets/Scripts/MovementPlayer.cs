75355f73c76c6d63b6e2214648b4d5678016768e
LorysChepy <lorys.chepy@supinfo.com>
2024-02-12 09:38:02 +0100

    normal

--- Age of war/Assets/Scripts/MovementPlayer.cs
+++ Age of war/Assets/Scripts/MovementPlayer.cs
@@ -6,6 +6,7 @@ public class MovePlayer : MonoBehaviour
     public float moveSpeed;
     public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
+    public Animator animator;
 
 
     void FixedUpdate()
@@ -13,11 +14,17 @@ public class MovePlayer : MonoBehaviour
         float horizontalMovement= Input.GetAxis("Horizontal") * moveSpeed* Time.deltaTime;
 
         MovePlayers(horizontalMovement);
+        float characterVelocity = Mathf.Abs(rb.velocity.x);
+        animator.SetFloat("speed",characterVelocity);
     }
     void MovePlayers(float _horizontalMovement)
 
+ 
+
 
     {
+        
+    
         Vector3 targetVelocity = new Vector2 (_horizontalMovement, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity,ref velocity, .05f);
     }

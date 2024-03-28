07d5a266d0b011bffa4f4c3a8eb570442b2d48a4
myannisbg <503p2.yannis.mercier@gmail.com>
2024-03-28 10:42:42 +0100

    langue opperationel
    langue opperationel et fonctionel et qui fonctionne tout marche c'est la fete

--- Age of war/Assets/Scripts/LanguageObject.cs
+++ Age of war/Assets/Scripts/LanguageObject.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.Localization.Settings;
+using System.Collections;
 
 public class LanguageObject : MonoBehaviour
 {
@@ -6,6 +8,7 @@ public class LanguageObject : MonoBehaviour
     private GameObject selectedButton; // Bouton de langue sélectionné
     private bool isUnfold = false;
     private float buttonSpacing = 50f;
+    private bool active = false;
     void Start()
     {
         // Au début, tous les boutons sont cachés sauf le premier
@@ -29,14 +32,44 @@ public class LanguageObject : MonoBehaviour
         }
         else{
             selectedButton = button;
+            string buttonName = selectedButton.gameObject.name;
+
             for (int i = 0; i < languageButtons.Length; i++)
             {
                 languageButtons[i].SetActive(false);
                 languageButtons[i].transform.position = transform.position;
             }
             selectedButton.SetActive(true);
+            if (buttonName == "france"){
+                ChangeLocale(1);
+            }
+            else if(buttonName == "anglais"){
+                ChangeLocale(0);
+            }
+            else if(buttonName == "espagne"){
+                ChangeLocale(4);
+            }
+            else if(buttonName == "italien"){
+                ChangeLocale(3);
+            }
+            else if(buttonName == "allemand"){
+                ChangeLocale(2);
+            }
+            print(selectedButton.gameObject.name);
             // selectedButton.transform.position = transform.position + Vector3.up * (languageButtons.Length - 1) * buttonSpacing;
             isUnfold = false;
         }
+    }
+
+    public void ChangeLocale(int localeID){
+        if (active == true)
+            return;
+        StartCoroutine(SetLocale(localeID));
+    }
+    IEnumerator SetLocale(int _localeID){
+        active = true;
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[_localeID];
+        active = false;
     }
 }

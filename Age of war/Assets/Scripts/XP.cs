850d5d62ef3ac4f1a56dcb97afe6b8a606aee17a
myannisbg <503p2.yannis.mercier@gmail.com>
2024-04-23 13:51:10 +0200

    gestion xp avec slider

---
+++ Age of war/Assets/Scripts/XP.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Xp : MonoBehaviour
+{
+    public int xp = 0;
+    public Slider xpBarre; // La référence à l'élément de texte UI
+    public int maxXP = 100;
+
+
+    void Start()
+    {
+        if (xpBarre == null)
+        {
+                Debug.LogError("Références non définies.dans XP.cs.");
+            return;
+        }
+
+        UpdateXpBarre(); // Met à jour le texte au démarrage
+    }
+    
+    public int getXp(){
+        return xp;
+    }
+
+    public void addXp(int amount){
+        xp += amount;
+        UpdateXpBarre(); // Mettre à jour le texte avec le nouveau montant
+    }
+
+    public void setMaxValue(int amount){
+        maxXP = amount;
+    }
+
+    public bool canBuy(int amount){
+
+        if (xp - amount < 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+
+    // Met à jour le texte avec le montant d'or
+    public void UpdateXpBarre()
+    {
+        xpBarre.maxValue = maxXP; 
+        xpBarre.value = xp;
+    }
+}

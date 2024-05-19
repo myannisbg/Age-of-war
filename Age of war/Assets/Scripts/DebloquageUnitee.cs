1c5420efd849222984cf3881b48477224d630a38
myannisbg <503p2.yannis.mercier@gmail.com>
2024-05-19 15:37:50 +0200

    base agrandis

---
+++ Age of war/Assets/Scripts/DebloquageUnitee.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebloquageUnitee : MonoBehaviour
+{
+    public Money argent;
+    public GlobalAge ageValue;
+    
+
+}

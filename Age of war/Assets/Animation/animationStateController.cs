2f2244a920e509725e452e23bca171547b127f45
LorysChepy <lorys.chepy@supinfo.com>
2024-05-30 14:55:58 +0200

    J AI MIS TOUTE LES ANIME

---
+++ Age of war/Assets/Animation/animationStateController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+public Animator animator;
+    void Start()
+    {
+    animator.SetBool("isWalking", false);
+    animator.SetBool("isAttacking", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    /*if(self.attackDetection != None) {
+        animator.SetBool("isWalking", false);
+        animator.SetBool("isAttacking", true);
+    }
+    else if(self.currentSpeed > 0.3) {
+        animator.SetBool("isAttacking",false);
+        animator.SetBool("isWalking", true);
+    }
+    else {
+        animator.SetBool("isWalking",false);
+        animator.SetBool("isAttacking", false);
+        }*/
+    }
+}

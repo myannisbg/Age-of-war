e2ec0c36ae9c8dc1d161b254134f8d3bb3d7afa8
myannisbg <503p2.yannis.mercier@gmail.com>
2024-05-30 11:32:43 +0200

    bind de unité 5 avec le juex + fond ecran egypte

--- Age of war/Assets/Scripts/BackgroundChange.cs
+++ Age of war/Assets/Scripts/BackgroundChange.cs
@@ -9,12 +9,16 @@ public class BackgroundChange : MonoBehaviour
     public GameObject fond3avant;
     public GameObject fond4avant;
     public GameObject fond5avant;
+    public GameObject fond6avant;
+    public GameObject fond7avant;
 
     public GameObject fond1arriere;
     public GameObject fond2arriere;
     public GameObject fond3arriere;
     public GameObject fond4arriere;
     public GameObject fond5arriere;
+    public GameObject fond6arriere;
+    public GameObject fond7arriere;
 
     public GlobalAge ageValue;
     // Update is called once per frame
@@ -29,12 +33,16 @@ public class BackgroundChange : MonoBehaviour
         fond3avant.SetActive(false);
         fond4avant.SetActive(false);
         fond5avant.SetActive(false);
+        fond6avant.SetActive(false);
+        fond7avant.SetActive(false);
 
         fond1arriere.SetActive(false);
         fond2arriere.SetActive(false);
         fond3arriere.SetActive(false);
         fond4arriere.SetActive(false);
         fond5arriere.SetActive(false);
+        fond6arriere.SetActive(false);
+        fond7arriere.SetActive(false);
 
         if (ageValue.getAge() == 0){
             fond1avant.SetActive(true);
@@ -55,6 +63,14 @@ public class BackgroundChange : MonoBehaviour
         else if (ageValue.getAge() == 4){
             fond5avant.SetActive(true);
             fond5arriere.SetActive(true);
+        }
+        else if (ageValue.getAge() == 5){
+            fond6avant.SetActive(true);
+            fond6arriere.SetActive(true);
+        }
+        else if (ageValue.getAge() == 6){
+            fond7avant.SetActive(true);
+            fond7arriere.SetActive(true);
         }
 
     }

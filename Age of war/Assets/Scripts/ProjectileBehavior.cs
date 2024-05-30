916a0b1cf57366050b6337989978dea262bf10a0
LorysChepy <lorys.chepy@supinfo.com>
2024-05-30 14:13:56 +0200

    les spécial corriger en partie

--- Age of war/Assets/Scripts/ProjectileBehavior.cs
+++ Age of war/Assets/Scripts/ProjectileBehavior.cs
@@ -6,8 +6,15 @@ public class ProjectileBehavior : MonoBehaviour
     public Unit.UnitType unitType;
     
 
-    private void OnTriggerEnter2D(Collider2D collision)
+     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Vérifier si le projectile touche un élément avec le tag "Special"
+        if (collision.CompareTag("Special"))
+        {
+            // Ne rien faire si le tag est "Special"
+            return;
+        }
+
         // Vérifier si le projectile touche un ennemi
         if (collision.CompareTag("Ennemy"))
         {
@@ -15,7 +22,7 @@ public class ProjectileBehavior : MonoBehaviour
             if (enemyUnit != null)
             {
                 // Infliger des dégâts à l'ennemi
-                enemyUnit.TakeDamage(specialAttackDamage,Unit.UnitType.None);
+                enemyUnit.TakeDamage(specialAttackDamage, Unit.UnitType.None);
             }
         }
 
